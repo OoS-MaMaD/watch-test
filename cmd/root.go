c5684e17fb1ea1d35679b0a1c0e9fdc5943fdb5d
Mohammad Mahdi Hojjati <62786813+OoS-MaMaD@users.noreply.github.com>
2024-12-10 16:09:22 +0330

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -7,10 +7,9 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "go-blueprint",
-	Short: "A program to spin up a quick Go project using a popular framework",
-	Long: `Go Blueprint is a CLI tool that allows users to spin up a Go project with the corresponding structure seamlessly. 
-It also gives the option to integrate with one of the more popular Go frameworks!`,
+	Use:   "watch-test",
+	Short: "test",
+	Long: `ttttttttttteeeeeeeeeeeeeeeeesssssssssssssssstttttttttttttttttttt`,
 }
 
 func Execute() {
@@ -21,6 +20,5 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.AddCommand(versionCmd)
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
